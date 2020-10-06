72380f40d85923b02877d0c3db2eaab5d4369bcd
Andy M. Newhouse <hi@andymnewhouse.me>
2020-10-06 11:50:44 -0500

    Add dark prose mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,9 +26,70 @@ module.exports = {
             '7xl': '5rem'
         }
     },
+    typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            '[class~="lead"]': {
+                color: theme('colors.gray.300'),
+            },
+            a: {
+                color: theme('colors.gray.100'),
+                textDecoration: 'underline',
+            },
+            strong: {
+                color: theme('colors.gray.100'),
+                fontWeight: '600',
+            },
+            'ol > li::before': {
+                color: theme('colors.gray.400'),
+            },
+            'ul > li::before': {
+                backgroundColor: theme('colors.gray.600'),
+            },
+            hr: {
+                borderColor: theme('colors.gray.700'),
+            },
+            blockquote: {
+                color: theme('colors.gray.100'),
+                borderLeftColor: theme('colors.gray.700'),
+            },
+            h1: {
+                color: theme('colors.gray.100'),
+            },
+            h2: {
+                color: theme('colors.gray.100'),
+            },
+            h3: {
+                color: theme('colors.gray.100'),
+            },
+            h4: {
+                color: theme('colors.gray.100'),
+            },
+            'figure figcaption': {
+                color: theme('colors.gray.400'),
+            },
+            code: {
+                color: theme('colors.gray.100'),
+            },
+            pre: {
+                color: theme('colors.gray.700'),
+                backgroundColor: theme('colors.gray.200'),
+            },
+            thead: {
+                color: theme('colors.gray.100'),
+                borderBottomColor: theme('colors.gray.600'),
+            },
+            'tbody tr': {
+                borderBottomColor: theme('colors.gray.700'),
+            },
+          },
+        },
+      }),
   },
   variants: {},
   plugins: [
+    require('@tailwindcss/typography'),
     require('@tailwindcss/ui'),
   ],
 }
