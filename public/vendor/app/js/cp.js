bc5e1ae14d7f1406b0df06d7f03aff29139eb5cf
Andy M. Newhouse <hi@andymnewhouse.me>
2021-01-12 19:29:48 -0600

    Add video component

--- public/vendor/app/js/cp.js
+++ public/vendor/app/js/cp.js
@@ -1,1 +1,219 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var u=t[r]={i:r,l:!1,exports:{}};return e[r].call(u.exports,u,u.exports,n),u.l=!0,u.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var u in e)n.d(r,u,function(t){return e[t]}.bind(null,u));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=1)}({1:function(e,t,n){e.exports=n("Gax2")},Gax2:function(e,t,n){"use strict";function r(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}n.r(t);var u=Statamic.$bard.tiptap.core;console.log(u);var o=function(){function e(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e)}var t,n,u;return t=e,(n=[{key:"name",value:function(){return"lead"}},{key:"schema",value:function(){return{parseDOM:[{tag:"p"}],toDOM:function(){return["p",{style:"color: #404040; font-size: 20px; display: block"},0]}}}},{key:"commands",value:function(e){var t=e.type,n=e.toggleMark;return function(){return n(t)}}},{key:"pasteRules",value:function(e){return e.type,[]}},{key:"inputRules",value:function(e){var t=e.type;return[markInputRule(/(?:\*\*|__)([^*_]+)(?:\*\*|__)$/,t)]}},{key:"plugins",value:function(){return[]}}])&&r(t.prototype,n),u&&r(t,u),e}();Statamic.$bard.extend((function(e){return(0,e.mark)(new o)})),Statamic.$bard.buttons((function(){return[{name:"lead",text:__("Lead"),command:"lead",icon:"paint-brush"}]}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/components/bard/Lead.js":
+/*!**********************************************!*\
+  !*** ./resources/js/components/bard/Lead.js ***!
+  \**********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Lead; });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var core = Statamic.$bard.tiptap.core;
+console.log(core);
+
+var Lead = /*#__PURE__*/function () {
+  function Lead() {
+    _classCallCheck(this, Lead);
+  }
+
+  _createClass(Lead, [{
+    key: "name",
+    value: function name() {
+      return "lead";
+    }
+  }, {
+    key: "schema",
+    value: function schema() {
+      return {
+        parseDOM: [{
+          tag: 'p'
+        }],
+        toDOM: function toDOM() {
+          return ["p", {
+            style: "color: #404040; font-size: 20px; display: block"
+          }, 0];
+        }
+      };
+    }
+  }, {
+    key: "commands",
+    value: function commands(_ref) {
+      var type = _ref.type,
+          toggleMark = _ref.toggleMark;
+      return function () {
+        return toggleMark(type);
+      };
+    }
+  }, {
+    key: "pasteRules",
+    value: function pasteRules(_ref2) {
+      var type = _ref2.type;
+      return [];
+    }
+  }, {
+    key: "inputRules",
+    value: function inputRules(_ref3) {
+      var type = _ref3.type;
+      return [markInputRule(/(?:\*\*|__)([^*_]+)(?:\*\*|__)$/, type)];
+    }
+  }, {
+    key: "plugins",
+    value: function plugins() {
+      return [];
+    }
+  }]);
+
+  return Lead;
+}();
+
+
+
+/***/ }),
+
+/***/ "./resources/js/cp.js":
+/*!****************************!*\
+  !*** ./resources/js/cp.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_bard_Lead__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/bard/Lead */ "./resources/js/components/bard/Lead.js");
+/**
+ * When extending the control panel, be sure to uncomment the necessary code for your build process:
+ * https://statamic.dev/extending/control-panel
+ */
+
+/** Example Fieldtype
+
+import ExampleFieldtype from './components/fieldtypes/ExampleFieldtype.vue';
+
+Statamic.booting(() => {
+    Statamic.$components.register('example-fieldtype', ExampleFieldtype);
+});
+
+*/
+
+Statamic.$bard.extend(function (_ref) {
+  var mark = _ref.mark;
+  return mark(new _components_bard_Lead__WEBPACK_IMPORTED_MODULE_0__["default"]());
+});
+Statamic.$bard.buttons(function () {
+  return [{
+    name: 'lead',
+    text: __('Lead'),
+    command: 'lead',
+    icon: 'paint-brush'
+  }];
+});
+
+/***/ }),
+
+/***/ 1:
+/*!**********************************!*\
+  !*** multi ./resources/js/cp.js ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/andymnewhouse/Development/sgdinstitute/apollo/resources/js/cp.js */"./resources/js/cp.js");
+
+
+/***/ })
+
+/******/ });
