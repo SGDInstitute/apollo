7201e9e81f9f1ad0e36f0d0e9a370f8ca06d6376
Andy M. Newhouse <hi@andymnewhouse.me>
2021-01-15 09:48:13 -0600

    Update list

--- app/Listeners/Subscribed.php
+++ app/Listeners/Subscribed.php
@@ -13,8 +13,7 @@ class Subscribed
     {
         if($event->submission->form()->handle() === 'newsletter') {
             $email = $event->submission->data()['email'];
-            $apiKey = 'Dn8MX7nbZJQUOqasjU2vapiMcTPssYrN1C4x6V7s';
-            $response = Http::withToken(config('services.mailcoach.token'))->post('https://mailcoach.sgdinstitute.org/api/email-lists/8/subscribers', ['email' => $email, 'tags' => ['website']]);
+            $response = Http::withToken(config('services.mailcoach.token'))->post('https://mailcoach.sgdinstitute.org/api/email-lists/23/subscribers', ['email' => $email, 'tags' => ['website']]);
         }
     }
 }
